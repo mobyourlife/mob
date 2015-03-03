ab4e1a4e04ef376e2f9254d56ed52d92e5ec6231
Filipe Oliveira <contato@fmoliveira.com.br>
2015-03-03 16:31:25 -0300

    Removendo as chamadas a API do Facebook na inicialização do servidor.

--- bin/server.js
+++ bin/server.js
@@ -2,12 +2,9 @@
 
 var debug = require('debug')('mob');
 var app = require('../app');
-var sync = require('../sync')();
 
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
-
-sync.syncAll();

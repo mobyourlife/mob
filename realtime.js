d682c55bee6f864ab815afd824a34ae8393ca40d
Filipe Oliveira <contato@fmoliveira.com.br>
2015-03-05 13:32:23 -0300

    Movendo os testes do realtime para o programa de teste.

--- realtime.js
+++ realtime.js
@@ -1,109 +1,11 @@
 var FB = require('fb');
-var auth = require('./config/auth');
 
 module.exports = function() {
     
-    var setAppAccessToken = function(callback) {
-        FB.api('oauth/access_token', {
-            client_id: auth.facebookAuth.clientID,
-            client_secret: auth.facebookAuth.clientSecret,
-            grant_type: 'client_credentials'
-        }, function(res) {
-            if (!res || res.error) {
-                console.log(!res ? 'Erro insperado!' : res.error);
-                return;
-            }
-
-            FB.setAccessToken(res.access_token);
-
-            if (callback) {
-                callback(res.access_token);
-            }
-        });
-    }
-    
-    var addAppToPage = function(pageid, callback) {
-        FB.api('/' + pageid + '/tabs', 'post', {
-            app_id: auth.facebookAuth.clientID
-        }, function(res) {
-            if (callback) {
-                callback(res);
-            }
-        });
-    }
-    
-    var listPageTabs = function(pageid, callback) {
-        FB.api('/' + pageid + '/tabs', function(res) {
-            if (callback) {
-                callback(res);
-            }
-        });
-    }
-    
-    var setSubscription = function(object, fields, callback) {
-        FB.api('/' + auth.facebookAuth.clientID + '/subscriptions', 'post', {
-            object: object,
-            callback_url: auth.facebookAuth.realtimeURL,
-            fields: fields,
-            verify_token: '123456'
-        }, function(res) {
-            if(callback) {
-                callback(res);
-            }
-        });
-    }
-    
-    var removeSubscription = function(object, callback) {
-        FB.api('/' + auth.facebookAuth.clientID + '/subscriptions', 'delete', {
-            object: object
-        }, function(res) {
-            if (callback) {
-                callback(res);
-            }
-        });
-    }
-
-    var listSubscriptions = function(callback) {
-        FB.api('/' + auth.facebookAuth.clientID + '/subscriptions', function(records) {
-            if (callback) {
-                callback(records);
-            }
-        });
-    }
-    
-    var subscribeApp = function(pageid, callback) {
-        FB.api('/v2.2/' + pageid + '/subscribed_apps', 'post', function(res) {
-            if (callback) {
-                callback(res);
-            }
-        });
-    }
-    
-    var unsubscribeApp = function(pageid, callback) {
-        FB.api('/v2.2/' + pageid + '/subscribed_apps', 'delete', function(res) {
-            if (callback) {
-                callback(res);
-            }
-        });
-    }
-    
-    var listApps = function(pageid, callback) {
-        FB.api('/v2.2/' + pageid + '/subscribed_apps', function(records) {
-            if (callback) {
-                callback(records);
-            }
-        });
+    var fetchObject = function(object_id) {
     }
 
     return {
-        setAppAccessToken: setAppAccessToken,
-        addAppToPage: addAppToPage,
-        listPageTabs: listPageTabs,
-        setSubscription: setSubscription,
-        removeSubscription: removeSubscription,
-        listSubscriptions: listSubscriptions,
-        subscribeApp: subscribeApp,
-        unsubscribeApp: unsubscribeApp,
-        listApps: listApps
+        fetchObject: fetchObject
     }
 }
--- test.js
+++ test.js
@@ -1,5 +1,98 @@
 var FB = require('fb');
-var realtime = require('./realtime')();
+var auth = require('./config/auth');
+
+var setAppAccessToken = function(callback) {
+    FB.api('oauth/access_token', {
+        client_id: auth.facebookAuth.clientID,
+        client_secret: auth.facebookAuth.clientSecret,
+        grant_type: 'client_credentials'
+    }, function(res) {
+        if (!res || res.error) {
+            console.log(!res ? 'Erro insperado!' : res.error);
+            return;
+        }
+
+        FB.setAccessToken(res.access_token);
+
+        if (callback) {
+            callback(res.access_token);
+        }
+    });
+}
+
+var addAppToPage = function(pageid, callback) {
+    FB.api('/' + pageid + '/tabs', 'post', {
+        app_id: auth.facebookAuth.clientID
+    }, function(res) {
+        if (callback) {
+            callback(res);
+        }
+    });
+}
+
+var listPageTabs = function(pageid, callback) {
+    FB.api('/' + pageid + '/tabs', function(res) {
+        if (callback) {
+            callback(res);
+        }
+    });
+}
+
+var setSubscription = function(object, fields, callback) {
+    FB.api('/' + auth.facebookAuth.clientID + '/subscriptions', 'post', {
+        object: object,
+        callback_url: auth.facebookAuth.realtimeURL,
+        fields: fields,
+        verify_token: '123456'
+    }, function(res) {
+        if(callback) {
+            callback(res);
+        }
+    });
+}
+
+var removeSubscription = function(object, callback) {
+    FB.api('/' + auth.facebookAuth.clientID + '/subscriptions', 'delete', {
+        object: object
+    }, function(res) {
+        if (callback) {
+            callback(res);
+        }
+    });
+}
+
+var listSubscriptions = function(callback) {
+    FB.api('/' + auth.facebookAuth.clientID + '/subscriptions', function(records) {
+        if (callback) {
+            callback(records);
+        }
+    });
+}
+
+var subscribeApp = function(pageid, callback) {
+    FB.api('/v2.2/' + pageid + '/subscribed_apps', 'post', function(res) {
+        if (callback) {
+            callback(res);
+        }
+    });
+}
+
+var unsubscribeApp = function(pageid, callback) {
+    FB.api('/v2.2/' + pageid + '/subscribed_apps', 'delete', function(res) {
+        if (callback) {
+            callback(res);
+        }
+    });
+}
+
+var listApps = function(pageid, callback) {
+    FB.api('/v2.2/' + pageid + '/subscribed_apps', function(records) {
+        if (callback) {
+            callback(records);
+        }
+    });
+}
+
 
 if (process.argv.length >= 3) {
     switch (process.argv[2]) {
@@ -7,8 +100,8 @@ if (process.argv.length >= 3) {
         case 'tabs':
             if (process.argv.length >= 5) {
                 FB.setAccessToken(process.argv[4]);
-                realtime.addAppToPage(process.argv[3], function(res) {
-                    realtime.listPageTabs(process.argv[3], function(tabs) {
+                addAppToPage(process.argv[3], function(res) {
+                    listPageTabs(process.argv[3], function(tabs) {
                         console.log(tabs);
                     });
                 });
@@ -21,8 +114,8 @@ if (process.argv.length >= 3) {
         case 'sign':
             if (process.argv.length >= 5) {
                 FB.setAccessToken(process.argv[4]);
-                realtime.subscribeApp(process.argv[3], function(res) {
-                    realtime.listApps(process.argv[3], function(apps) {
+                subscribeApp(process.argv[3], function(res) {
+                    listApps(process.argv[3], function(apps) {
                         console.log(apps);
                     });
                 });
@@ -35,8 +128,8 @@ if (process.argv.length >= 3) {
         case 'unsign':
             if (process.argv.length >= 5) {
                 FB.setAccessToken(process.argv[4]);
-                realtime.unsubscribeApp(process.argv[3], function(res) {
-                    realtime.listApps(process.argv[3], function(apps) {
+                unsubscribeApp(process.argv[3], function(res) {
+                    listApps(process.argv[3], function(apps) {
                         console.log(apps);
                     });
                 });
@@ -47,12 +140,8 @@ if (process.argv.length >= 3) {
             
         /* add subscriptions */
         case 'add':
-            realtime.setAppAccessToken(function(token) {
-                realtime.setSubscription('page', 'feed, name, picture, category, description, founded, company_overview, conversations, mission, products, general_info, location, hours, parking, public_transit, phone, email, website, attire, payment_options, culinary_team, general_manager, price_range, restaurant_services, restaurant_specialties, videos, release_date, genre, starring, screenplay_by, directed_by, produced_by, studio, awards, plot_outline, network, season, schedule, written_by, band_members, hometown, current_location, record_label, booking_agent, press_contact, artists_we_like, influences, band_interests, bio, affiliation, birthday, personal_info, personal_interests, members, built, features, mpg, checkins, productlists', function(ret) {
-                    console.log('setSubscription:');
-                    console.log(ret);
-                });
-                realtime.setSubscription('user', 'about, about_me, activities, birthday, birthday_date, books, checkins, contact_email, current_location, email, email_hashes, events, feed, first_name, friends, has_added_app, hometown, hometown_location, interests, is_app_user, is_blocked, last_name, likes, link, locale, location, movies, music, name, photos, pic, picture, political_views, profile_blurb, profile_update_time, profile_url, proxied_email, quotes, timezone, television, tv, videos, website, status', function(ret) {
+            setAppAccessToken(function(token) {
+                setSubscription('page', 'feed, name, picture, category, description, founded, company_overview, conversations, mission, products, general_info, location, hours, parking, public_transit, phone, email, website, attire, payment_options, culinary_team, general_manager, price_range, restaurant_services, restaurant_specialties, videos, release_date, genre, starring, screenplay_by, directed_by, produced_by, studio, awards, plot_outline, network, season, schedule, written_by, band_members, hometown, current_location, record_label, booking_agent, press_contact, artists_we_like, influences, band_interests, bio, affiliation, birthday, personal_info, personal_interests, members, built, features, mpg, checkins, productlists', function(ret) {
                     console.log('setSubscription:');
                     console.log(ret);
                 });
@@ -61,8 +150,8 @@ if (process.argv.length >= 3) {
         
         /* list subscriptions */
         case 'list':
-            realtime.setAppAccessToken(function(token) {
-                realtime.listSubscriptions(function(records) {
+            setAppAccessToken(function(token) {
+                listSubscriptions(function(records) {
                     console.log('listSubscriptions');
                     console.log(records);
                 });
@@ -71,11 +160,8 @@ if (process.argv.length >= 3) {
         
         /* delete subscriptions */
         case 'rm':
-            realtime.setAppAccessToken(function(token) {
-                realtime.removeSubscription('page', function(ret) {
-                    console.log('removeSubscription: ' + ret);
-                });
-                realtime.removeSubscription('user', function(ret) {
+            setAppAccessToken(function(token) {
+                removeSubscription('page', function(ret) {
                     console.log('removeSubscription: ' + ret);
                 });
             });

63484bdebae8d4c9594f5043f7da19a358507efa
Filipe Oliveira <contato@fmoliveira.com.br>
2015-03-06 11:25:44 -0300

    Processando os registros em ordem de data.

--- realtime.js
+++ realtime.js
@@ -17,7 +17,7 @@ module.exports = function() {
         var pending = Array();
         
         if (callback) {
-            RealtimeUpdate.find({ "updated": { '$ne': true } }, function(err, found) {
+            RealtimeUpdate.find({ "updated": { '$ne': true } }).sort({ "data.entry.0.time": 1 }).exec(function(err, found) {
                 if (err)
                     throw err;
 
@@ -25,31 +25,47 @@ module.exports = function() {
                     for (i = 0; i < found.length; i++) {
                         var row = found[i];
 
-                        for (j = 0; j < row.data.entry.length; j++) {
-                            var entry = row.data.entry[j];
+                        if (row.data.entry && row.data.entry != 0) {
+                            for (j = 0; j < row.data.entry.length; j++) {
+                                var entry = row.data.entry[j];
 
-                            for (k = 0; k < entry.changes.length; k++) {
-                                var change = entry.changes[k];
-                                var type = row.data.object + '.' + change.field + '.' + change.value.verb + '.' + change.value.item;
-                                pending.push({
-                                    type: type,
-                                    page_id: entry.id,
-                                    time: entry.time,
-                                    value: change.value
-                                });
+                                if (entry.changes && entry.changes.length != 0) {
+                                    for (k = 0; k < entry.changes.length; k++) {
+                                        var change = entry.changes[k];
+                                        var type = row.data.object + '.' + change.field + '.' + change.value.verb + '.' + change.value.item;
+                                        pending.push({
+                                            type: type,
+                                            page_id: entry.id,
+                                            time: entry.time,
+                                            value: change.value
+                                        });
+                                    }
+                                } else {
+                                    console.log('No changes found for:');
+                                    console.log(row.data);
+                                }
                             }
+                        } else {
+                            console.log('No entries found for:');
+                            console.log(row.data);
                         }
                     }
                 }
 
-                    callback(pending);
+                callback(pending);
             });
         } else {
             console.log('Error: No callback was supplied to realtime.getPending! Aborting operation.');
         }
     }
     
-    var fetchObject = function(object_id) {
+    var addPhoto = function() {
+    }
+    
+    var removePhoto = function() {
+    }
+    
+    var downloadPhoto = function() {
     }
     
     var syncPending = function() {
